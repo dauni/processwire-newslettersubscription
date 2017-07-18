e00ba975a35e50070dc03e4b2e47276644b0013a
dauni <pichler.daniel@gmail.com>
2017-07-18 10:18:03 +0200

    Added HTML-Templates for subscription and unsubscription-email Fixed deactivation instead of deletion

---
+++ tpl/subscribe.php
@@ -0,0 +1,5 @@
+Hey <?= $name ?>,<br>
+
+Please <a href="<?= $link ?>">click here</a> the following link to confirm your subscription.<br>
+
+You can copy and paste this URL in you Browser: <?= $link ?>
---
+++ tpl/unsubscribe.php
@@ -0,0 +1,3 @@
+Hey <?= $name ?>,<br><br>
+
+Please click the following link to end your subscription: <a href="<?= $link ?>"><?= $link ?></a>
